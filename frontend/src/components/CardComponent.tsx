7924d8d9def19d69827b8353c8f5c92de03c7b82
Ariya <catzilaza@gmail.com>
2023-03-26 15:45:10 +0700

    update 26/3/66

--- frontend/src/components/CardComponent.tsx
+++ frontend/src/components/CardComponent.tsx
@@ -1,10 +1,54 @@
 import { dataProducts } from "../data";
 import { Card, Button, Row, Col } from "react-bootstrap";
+import axios from "axios";
+import React, { useState } from "react";
+
+interface dessertData {
+  dessert_name: string;
+  dessert_image: string;
+  dessert_price: number;
+}
+
 function CardComponent() {
+  //const [data, setData] = useState<any>([]);
+  const [data, setData] = useState<dessertData[]>([]);  
+  const [error, setError] = useState<any>(null);
+  //http://localhost:5000/api/desserts
+  async function getAlldata() {
+    await axios
+      .get("https://blue-jolly-snail.cyclic.app/api/desserts")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+        setError(error);
+      });
+  }
+  React.useEffect(() => {
+    getAlldata();
+  }, []);
+
+  //console.log("Front : ", data)
+  // data.map((item)=>{
+  //   console.log("item : ", item.dessert_image)
+  // })
+
+  if (error) return `Error: ${error}`;
+  if (!data)
+    return (
+      <>
+        "Loading data!"
+        <div className="spinner-border text-primary" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </>
+    );
+
   return (
     <>
       <Row>
-        {dataProducts.map((product, id) => (
+        {data.map((product, id) => (
           <Col key={id}>
             <Card
               border="info"
@@ -12,16 +56,16 @@ function CardComponent() {
             >
               <Card.Img
                 variant="top"
-                src={product.image}
-                alt={product.name}
+                src={product.dessert_image}
+                alt={product.dessert_name}
                 width="100%"
                 height="100%"
               />
               <Card.Body>
-                <Card.Title>{product.name}</Card.Title>
-                <Card.Text>ราคา {product.price} บาท</Card.Text>
+                <Card.Title>{product.dessert_name}</Card.Title>
+                <Card.Text>ราคา {product.dessert_price} บาท</Card.Text>
                 <div className="d-grid">
-                  <Button href={`product/${id}`} variant="primary" size="lg" >
+                  <Button href={`product/${id}`} variant="primary" size="lg">
                     ซื้อ
                   </Button>
                 </div>
